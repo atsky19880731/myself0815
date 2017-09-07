9853cea60371f838b1f7deed624488cd28cf658a
sakata <americandream066@gmail.com>
2017-09-07 21:53:50 +0800

    test myself

---
+++ bolt/theme/_index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Yutaka Introdece</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/chart.js"></script>
+    <script src="assets/js/link.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="https://www.youtube.com/?gl=JP"><i class="fa fa-headphones" aria-hidden="true"></i></a>
+        </div>
+        <div class="navbar-collapse collapse">
+          
+          <ul class="nav navbar-nav navbar-left1">
+            <li class="active"><a href="http://www.standardsmile.com/">BuyClothes</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-left1">
+            <li class="active"><a href="https://overall.amebaownd.com/">HairCut</a></li>
+          </ul>
+          
+          <ul class="nav navbar-nav navbar-left1">
+            <li class="active"><a href="http://www.city.tajimi.lg.jp/">MyCity</a></li>
+          </ul>
+          
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+<div class="container link ">
+  <div class="row">
+
+  </div>
+</div>
+  <div id="edithello">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 centered">
+          <h1>  
+           <a href="" class="typewrite" data-period="2000" data-type='[ "Hi, Im yutaka Sakata.", "Just Do It.", "You cant succeed if you dont work." ]'>
+            <span class="wrap"></span>
+          </a>
+          </h1>
+            <h1>坂田　諭隆</h1>
+            <h3>出身</h3>
+            <p>Gifu,Japan,Tajimi-City<br></p>
+            <h3>趣味</h3>
+            <p>Futsal,Snowbowding,Home Drunk<br></p>
+            <h3>Self Coaching Work</h3>
+            <p>自分の力で世界を相手にできるエンジニアになる（１０年後<br>
+               留学卒業後にはある程度自分でできるエンジニアになる<br>
+               留学中エンジニアとして足りない所を明確にする<br>
+               ダイビングライセンスを取る<br>
+               仲間とバカやったり(程々にw)とにかく楽しむ！</p>
+          </div>
+          </div><!-- /col-lg-12 -->
+        </div><!-- /row -->
+      </div> <!-- /container -->
+  </div><!-- /edithello -->
+  
+
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+      <h2 class="text-center">Contact</h2>
+        <form method="POST" action="">
+          <div class="form-group">
+          <label>nickname</label>
+          <input class="form-control" type="text" name="nickname" required>
+          </div>
+
+           <div class="form-group">
+          <label>email</label>
+          <input class="form-control" type="email" name="email" required>
+          </div>
+
+          <div class="form-group">
+            <label>message</label>
+            <textarea class="form-control" name="content" row="10" required></textarea>
+          </div>
+
+          <div class="form-group">
+            <button type="submit" class="send-color">send</button>
+          </div>
+        </form>
+
+        <?php if(!empty($_POST)) { ?>
+            <div class="alert alert-success">
+            <strong>お問い合わせ完了！</strong>
+              返答まで少しお待ちください。
+              </div>
+        <?PHP } ?>
+
+
+      
+
+      </div>
+    </div>
+  </div>
+  
+  <section id="contact"></section>
+  <div id="social">
+    <div class="container">
+      <div class="row centered">
+        <div class="col-lg-12 centered">
+          <div class="col-md-3">
+            <a href="https://www.facebook.com/sakata.yutaka"><i class="fa fa-facebook"></i>profile</a>
+          </div>
+          <div class="col-md-3">
+            <a href="https://www.instagram.com/atsky19880731/"><i class="fa fa-instagram" aria-hidden="true"></i>photo</a>
+          </div>
+          <div class="col-md-3">
+            <a href="tweitter.html"><i class="fa fa-twitter"></i>click here!</a>
+          </div>
+          <div class="col-md-3">
+            <a href="https://www.google.co.jp/maps/place/%E5%A4%9A%E6%B2%BB%E8%A6%8B%E9%A7%85/@35.3351588,137.1208405,16.05z/data=!4m13!1m7!3m6!1s0x60036a15b21266fd:0x5061049ae2df8e9e!2z44CSNTA3LTA4MjYg5bKQ6Zic55yM5aSa5rK76KaL5biC6ISH5LmL5bO255S677yU5LiB55uu77yS77yU4oiS77yS77yV!3b1!8m2!3d35.3181203!4d137.1189087!3m4!1s0x60036a66aefcc1e9:0xbcf87941dacaf335!8m2!3d35.3349792!4d137.1210413"><i class="fa fa-map-marker" aria-hidden="true"></i>adress</a>
+        </div>
+      </div>
+    </div><!-- /container -->
+  </div><!-- /social -->
+
+
+
+
+  <div id="f">
+    <div class="container">
+      <div class="row">
+        <p>Created by Yutaka.co.<i class="fa fa-money" aria-hidden="true"></i></p>
+      </div>
+    </div>
+  </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/bootstrap.js"></script>
+  </body>
+</html>
